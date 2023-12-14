f291873dca63836d5ee9aa707b2c81c859c1729a
Ralph-hue269 <120757645+Ralph-hue269@users.noreply.github.com>
2023-12-14 10:00:21 +0000

    Worked on the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Container, Row, Col, Button ,Image} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MyNav from './nav';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <MyNav />
+    <div className="Appy">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="https://pluspng.com/img-png/png-student-thinking-students-900.jpg" alt="A student thinking"  className="img-fluid" />
+        <Container className="holder" >
+          <Row className="text">
+      
+            <Col md={8} >
+              <h1>Missing Grade Reporting System</h1>
+              <p className="lead">
+                Welcome to the system designed to help you track and report missing grades.
+              </p>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col md={4} className="text-center">
+              <Button variant="primary" size="lg" href="/login">
+                Log In
+              </Button>
+            </Col>
+            <Col md={4} className="text-center">
+              <Button variant="success" size="lg" href="/report-missing-grades">
+                Report Missing Grades
+              </Button>
+            </Col>
+            <Col md={4} className="text-center">
+              <Button variant="info" size="lg" href="/contact-us">
+                Contact Us
+              </Button>
+            </Col>
+          </Row>
+        </Container>
       </header>
+    </div>
+    < Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './login'; // Import the default export
+import ContactUsPage from './ContactUsPage';
+import GradePage from './GradePage';
+import MissingGrades from './MissingGradeForm';
+import Dashboard from './Dashboard';
 import reportWebVitals from './reportWebVitals';
+import HelpAndSupport from './HelpAndSupport';
+import LandingPage from './LandingPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+function MyRouter() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/contact-us' element={<ContactUsPage />} />
+          <Route path='/faq' element={<HelpAndSupport />} />
+          <Route path='/grades' element={<GradePage />} />
+          <Route path='/missing-grade-form' element={<MissingGrades />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/landingpage' element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<MyRouter />);
+
 reportWebVitals();
